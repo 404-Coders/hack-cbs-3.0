c3041bd3c22e4bbba53dc141fcce390dc5c8ee39
Aastha Bhasin <62609764+aastha-b@users.noreply.github.com>
2020-11-08 11:32:48 +0530

    Login Page

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href='./css/header.css'>
     <link rel="stylesheet" href="./css/styles.css">
     <link rel="stylesheet" href="./css/searchbox.css">
     <link rel="stylesheet" href="./css/card.css">
